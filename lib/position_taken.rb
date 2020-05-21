98e73806a8c58e6f6f4d2ba07113eec11f32c206
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-05-21 21:04:21 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 def position_taken?(board, index)
   if board[index] == " " || "" return false && if board[index] == "X" || "O" return true
-    false
-  elsif board[index] == "X" || "O"
-    true
+  #   false
+  # elsif board[index] == "X" || "O"
+  #   true
   end
 
 
